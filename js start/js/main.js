2979555a26daa25edb15ca41f1b275cf068cb9be
Oleh0 <lavrekhaoleg@gmail.com>
2024-01-10 17:59:31 +0200

    svalka

---
+++ js start/js/main.js
@@ -0,0 +1,14 @@
+let test1 = 10
+let test2 = 14
+console.log(
+    test1 + test2 + 15
+    );
+
+let elDiv = document.querySelector('.test');
+console.log( elDiv.innerHTML )
+
+let a = elDiv.innerHTML;
+elDiv.innerHTML = 'ghf';
+
+console.log('a:');
+console.log('elDiv:', elDiv.innerHTML);
